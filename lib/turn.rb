601e871d31a67eabd003480be46ae32587b59e55
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-18 23:07:21 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -26,5 +26,7 @@ def turn(board)
   puts "Please enter 1-9: "
   input = gets.strip
   index = input_to_index(input)
-  until valid_move?()
+  until valid_move?(board, index)
+    turn(board)
+  end
 end
